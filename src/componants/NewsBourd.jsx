0d0ed9f7206a5c96b8742a86979dd9487879f326
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-07-29 14:18:46 +0530

    Add new api key

--- src/componants/NewsBourd.jsx
+++ src/componants/NewsBourd.jsx
@@ -7,7 +7,7 @@ export default function NewsBourd({category,country}) {
     const [articles, setArticles] = useState([]);
 // ${import.meta.env.NEWS_API_KEY}
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=65b6b16f6a2c4b428bb31f1e5b1e54b1`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=5dd955040a2641f0af2ab9e50950aacc`;
 
         fetch(url).then(
             response => response.json()
